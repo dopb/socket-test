fd6704f8509b6edfe0dc15a974daf6529a2ff770
Diego Paredes Bassi <diegoparedesb@gmail.com>
2015-12-06 19:46:15 -0300

    Added message emiter

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,10 @@
 var socket = io();
 
-io.on('connect', function () {
+socket.on('connect', function () {
     console.log('Connected to server')
+});
+
+socket.on('message', function (message) {
+    console.log('New message:');
+    console.log(message.text);
 });
